2e23f91769c88d65fe4dd184ca7964f8c80b4a0b
JulienZer <48547584+JulienZer@users.noreply.github.com>
2019-10-14 10:45:03 +0200

    Add files via upload

---
+++ Accueil technicien.php
@@ -0,0 +1,16 @@
+<php  
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Accueil Technicien</title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<u><h1 style="text-align: center;">Accueil technicien</h1></u>	
+	<br>
+	<center><a href="https://www.google.fr" target="_blank"><input type="button" name="Intervention Validation" value="Intervention / Validation" ></a></center>
+</body>
+</html>
