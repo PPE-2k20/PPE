ed2ffcc2c423252c2c526186a092fea621d73241
JulienZer <48547584+JulienZer@users.noreply.github.com>
2019-12-16 08:30:54 +0100

    Update InterV_T.php

--- InterV_T.php
+++ InterV_T.php
@@ -1,3 +1,4 @@
+  
 <?php 
 session_start();
   if (!isset ($_SESSION['login'])) {
@@ -7,31 +8,23 @@ session_start();
     header("location: accueil_A.php");
     //Si un assistant essaie d'acceder aux page technicien il est renvoyé vers la page assistant
   }
-
   $bdd = mysqli_connect("localhost","root","","ppe");
-
   $reqMatTechnicien = "SELECT technicien.matricule FROM technicien, utilisateur WHERE technicien.matricule = utilisateur.matricule and utilisateur.login =\"".$_SESSION['login']."\"";
   $resultMatTechnicien = mysqli_query($bdd,$reqMatTechnicien);
   $matT = $resultMatTechnicien ->fetch_array(MYSQLI_ASSOC);
-
   $req = "SELECT intervention.*, client.* FROM intervention, client, technicien WHERE client.numero_client = intervention.numero_client and intervention.matricule_technicien=technicien.matricule and intervention.validation = 0 and technicien.matricule= \"".$matT['matricule']."\" ORDER BY numero_intervention asc";
   $result=mysqli_query($bdd,$req);
-
     if(isset($_POST['valider1'])){
         $_SESSION['total'] = $_POST['total'];
         $_SESSION['numMachine'] = $_POST['numMachine'];
         $_SESSION['intervention'] = $_POST['intervention'];
     }
-
     if(isset($_POST['valider2'])){
-
       $_SESSION['total'] = $_SESSION['total'] + $_POST['ajouter']; 
-
       if(isset($_POST['numSerie'.($_SESSION['total']-1)]) and isset($_POST['Commentaire'.($_SESSION['total']-1)]) and isset($_POST['nbHeure'.($_SESSION['total']-1)])){ 
         $numSerie = $_POST["numSerie".($_SESSION['total']-1)];
         $Commentaire = $_POST['Commentaire'.($_SESSION['total']-1)];
         $nbHeure = $_POST['nbHeure'.($_SESSION['total']-1)];
-
         $infoInter = explode (" | ", $_SESSION['intervention']);
         $num_Inter = $infoInter[0];
         
@@ -39,11 +32,9 @@ session_start();
         $resultControl=mysqli_query($bdd,$reqControl);
         $result=mysqli_query($bdd,$req);
       }
-
       if($_SESSION['total'] == $_SESSION['numMachine']){
         $infoInter = explode (" | ", $_SESSION['intervention']);
         $num_Inter = $infoInter[0];
-
         $reqValidation = "UPDATE intervention SET Validation= 1 Where numero_intervention = \"".$num_Inter."\"";
         $result=mysqli_query($bdd,$reqValidation);
         
@@ -54,9 +45,6 @@ session_start();
         <?php 
         }
     }
-
-
-
   include_once 'db_connect.php';
 ?>
 
@@ -67,21 +55,22 @@ session_start();
     <meta charset="utf-8">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   </head>
 
   <body>
     <form id="form1" name="form1" method="post" action="">
       <div id = "divInter">
-      	<select multiple class="form-control col-6" size = 5 name="intervention" id="intervention">
-          	<?php while ($affiche = $result -> fetch_array(MYSQLI_ASSOC)) {?>
-          	    <option><?php echo $affiche['numero_intervention']." | ".$affiche['date_visite']." | ".$affiche['heure_visite']." | ".$affiche['nomC']." ".$affiche['prenomC']?></option>
-         		 <?php } ?>
-      	</select>
+        <select multiple class="form-control col-6" size = 5 name="intervention" id="intervention">
+            <?php while ($affiche = $result -> fetch_array(MYSQLI_ASSOC)) {?>
+                <option><?php echo $affiche['numero_intervention']." | ".$affiche['date_visite']." | ".$affiche['heure_visite']." | ".$affiche['nomC']." ".$affiche['prenomC']?></option>
+             <?php } ?>
+        </select>
         <input type="number" class="form-control" name="numMachine" id="numMachine" placeholder="Nombre de machine" min="1">
         <input type="number" name="total" value="0" hidden>
         <button type="submit" id="btnVal2" class="btn btn-primary" name="valider1">Valider</button> 
+        <input type="button" onclick="location.href='logout.php' ;" name="btnRetour" class="btn btn-primary" value="Déconnexion" />
       </div>
 
     <?php
@@ -92,9 +81,9 @@ session_start();
                 document.getElementById("intervention").setAttribute("disabled", true);
                 document.getElementById("numMachine").setAttribute("disabled", true);
                 document.getElementById("btnVal2").setAttribute("disabled", true);
+                document.getElementById("btnRetour").setAttribute("disabled", true);
             </script>
         <?php 
-
         if($_SESSION['total'] < $_SESSION['numMachine']){?>
             <div class="form-group">
               <input type="number" class="form-control" name="<?php echo "numSerie".$_SESSION['total'] ?>" placeholder="Numéro de série" min="1" required>
